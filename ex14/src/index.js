45171bd64af22bc1cf99326ce7c4f638683dd338
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-12 23:38:45 +0100

    Dodavanje ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    // Only change code below this line
+ var myNoun = "dog";
+ var myVerb = "ran";
+ var myAdjective = "big";
+ var myAdverb = "quickly";
+    wordBlanks ="My " + myNoun + " " + myVerb + " everyday and eats " + myAdjective + " apple very " + myAdverb + ".";
+    // Only change code above this line
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
