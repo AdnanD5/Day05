d91f23a14b06960a9982f7dc9bbee29ef2a503dd
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-12 22:25:20 +0100

    Dodavanje ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var myName = "Jason";
+    var myString = "Hello, my name is " + myName + ", how are you?";
+        
+    // Only change code above this line  
+    return myString
+ }
+ console.log(main());
+ module.exports = main;
