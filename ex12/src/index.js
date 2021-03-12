5c953510048b46613abd1d345f492a103eb25346
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-12 23:13:47 +0100

    Dodavanje ex12

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Tesla";
+    var lastLetterofLastName = lastName[lastName.length - 1];
+     
+    // Only change code above this line  
+    return lastLetterofLastName;
+ }
+ console.log(main());
+ module.exports = main;
