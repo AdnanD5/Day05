b9b11cdf7c086dab9b70dae06660acc6cd1aa48e
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-12 23:21:36 +0100

    Dodavanje

---
+++ ex13/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Bezos";
+    var thirdToLastLetterofLastName = lastName[lastName.length - 3];
+     
+    // Only change code above this line  
+    return thirdToLastLetterofLastName;
+ }
+ console.log(main());
+ module.exports = main;
